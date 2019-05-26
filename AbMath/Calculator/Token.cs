1cff638644f066b476c0eff666e961070cbf573d
Abhishek Sathiabalan <asathiabalan@gmail.com>
2019-05-26 17:41:14 -0400

    Moved more code into the Tokens class

--- AbMath/Calculator/Token.cs
+++ AbMath/Calculator/Token.cs
@@ -16,6 +16,76 @@ namespace AbMath.Calculator
             {
                 return Value;
             }
+
+            public bool IsNumber()
+            {
+                return Type == Type.Number;
+            }
+
+            public bool IsNull()
+            {
+                return Type == Type.Null;
+            }
+
+            public bool IsFunction()
+            {
+                return Type == Type.Function;
+            }
+
+            public bool IsConstant()
+            {
+                return IsFunction() && Arguments == 0;
+            }
+
+            public bool IsOperator()
+            {
+                return Type == Type.Operator;
+            }
+
+            public bool IsVariable()
+            {
+                return Type == Type.Variable;
+            }
+
+            public bool IsLeftBracket()
+            {
+                return Type == Type.LParen;
+            }
+
+            public bool IsRightBracket()
+            {
+                return Type == Type.RParen;
+            }
+
+            public bool IsComma()
+            {
+                return Value == ",";
+            }
+
+            public bool IsAddition()
+            {
+                return Value == "+";
+            }
+
+            public bool IsSubtraction()
+            {
+                return Value == "-";
+            }
+
+            public bool IsDivision()
+            {
+                return Value == "/";
+            }
+
+            public bool IsMultiplication()
+            {
+                return Value == "*";
+            }
+
+            public bool IsExponent()
+            {
+                return Value == "^";
+            }
         }
     }
 }
