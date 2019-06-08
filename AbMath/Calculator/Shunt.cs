4c21a32a3e747133cebe81b7cbf3f1073d7b83b9
Abhishek Sathiabalan <asathiabalan@gmail.com>
2019-06-07 23:58:05 -0400

    Removed redudant code
    Removed redudant using statements

--- AbMath/Calculator/Shunt.cs
+++ AbMath/Calculator/Shunt.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Diagnostics;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using CLI;
 
 namespace AbMath.Calculator
