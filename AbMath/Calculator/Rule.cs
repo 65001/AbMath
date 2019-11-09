f8efbbb9da361dae41763e83e2676cbafb3b6bfc
Abhishek Sathiabalan <asathiabalan@gmail.com>
2019-11-09 09:25:50 -0500

    Added a rule class

---
+++ AbMath/Calculator/Rule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbMath.Calculator
+{
+    /// <summary>
+    /// The rules class has been created to reduce complexity of the code
+    /// </summary>
+    public class Rule
+    {
+        public delegate bool isRunnable(RPN.Node node);
+        public delegate void Run(RPN.Node node);
+
+        public isRunnable CanRun { get; private set; }
+        public Run Compute { get; private set; }
+
+        public string Name { get; private set; }
+
+        public Rule(isRunnable isRunnable, Run run, string name)
+        {
+            CanRun = isRunnable;
+            Compute = run;
+            Name = name;
+        }
+
+        public bool Execute(RPN.Node node)
+        {
+            if (!CanRun.Invoke(node))
+            {
+                return false;
+            }
+
+            Compute.Invoke(node);
+            return true;
+        }
+    }
+}
