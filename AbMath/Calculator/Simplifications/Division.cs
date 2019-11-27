789c43fd16a9d28ad10dc06bff199c46fc6ed6ae
Abhishek Sathiabalan <asathiabalan@gmail.com>
2019-11-26 22:58:47 -0500

    Update Division.cs

--- AbMath/Calculator/Simplifications/Division.cs
+++ AbMath/Calculator/Simplifications/Division.cs
@@ -20,5 +20,21 @@ namespace AbMath.Calculator.Simplifications
         {
             return node[1];
         }
+
+        public static bool GCDRunnable(RPN.Node node)
+        {
+            return node.Children[0].IsInteger() && node.Children[1].IsInteger();
+        }
+
+        public static RPN.Node GCD(RPN.Node node)
+        {
+            double num1 = node.Children[0].GetNumber();
+            double num2 = node.Children[1].GetNumber();
+            double gcd = RPN.DoFunctions.Gcd(new double[] { num1, num2 });
+
+            node.Replace(node.Children[0], new RPN.Node((num1 / gcd)));
+            node.Replace(node.Children[1], new RPN.Node((num2 / gcd)));
+            return node;
+        }
     }
 }
