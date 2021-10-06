24e05b4f7016544a6202a9260fc2b584d4eb12f5
65001 <asathiabalan@gmail.com>
2021-10-05 20:48:55 -0400

    Refactored sum

--- AbMath/Calculator/Simplifications/Sum.cs
+++ AbMath/Calculator/Simplifications/Sum.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Numerics;
 using System.Text;
+using AbMath.Calculator.Operators;
 
 namespace AbMath.Calculator.Simplifications
 {
@@ -44,8 +45,7 @@ namespace AbMath.Calculator.Simplifications
                 nu[0] = -1;
             }
 
-            return new RPN.Node(new[] {new RPN.Node((double) de[0]), new RPN.Node((double) nu[0])},
-                new RPN.Token("/", 2, RPN.Type.Operator));
+            return new Div(new RPN.Node((double)nu[0]), new RPN.Node((double)de[0]));
         }
 
         public static bool PropagationRunnable(RPN.Node node)
@@ -55,13 +55,9 @@ namespace AbMath.Calculator.Simplifications
 
         public static RPN.Node Propagation(RPN.Node node)
         {
-            RPN.Token addToken = new RPN.Token("+", 2, RPN.Type.Operator);
-
             RPN.Node sum = new RPN.Node(new RPN.Node[] { node[0].Clone(), node[1].Clone(), node[2].Clone(), node[3, 1].Clone() }, _sum);
             node.Replace(node[3], node[3,0]); //This saves a simplification step later
-            RPN.Node addition = new RPN.Node(new RPN.Node[] { sum, node.Clone() }, addToken);
-
-            return addition;
+            return new Add(node.Clone(), sum);
         }
 
         public static bool VariableRunnable(RPN.Node node)
@@ -71,13 +67,7 @@ namespace AbMath.Calculator.Simplifications
 
         public static RPN.Node Variable(RPN.Node node)
         {
-            RPN.Node one = new RPN.Node(1);
-            RPN.Node two = new RPN.Node(2);
-
-            RPN.Node addition = new RPN.Node(new RPN.Node[] {node[0].Clone(), one}, new RPN.Token("+", 2, RPN.Type.Operator));
-            RPN.Node multiplication = new RPN.Node(new RPN.Node[] {addition, node[0]}, new RPN.Token("*", 2, RPN.Type.Operator));
-            RPN.Node division = new RPN.Node(new RPN.Node[] {two, multiplication}, new RPN.Token("/", 2, RPN.Type.Operator));
-            return division;
+            return new Div( new Mul(node[0], new Add(node[0].Clone(), new RPN.Node(1)) ), new RPN.Node(2) );
         }
 
         public static bool ConstantComplexRunnable(RPN.Node node)
@@ -100,10 +90,8 @@ namespace AbMath.Calculator.Simplifications
 
         public static RPN.Node Coefficient(RPN.Node node)
         {
-            RPN.Node sum = new RPN.Node(new RPN.Node[] {node[0].Clone(), node[1].Clone(), node[2].Clone(), node[3, 0]}, _sum);
-            RPN.Node multiplication = new RPN.Node(new RPN.Node[] {sum, node[3, 1] }, new RPN.Token("*", 2, RPN.Type.Operator));
-            
-            return multiplication;
+            RPN.Node sum = new RPN.Node(new RPN.Node[] {node[0].Clone(), node[1].Clone(), node[2].Clone(), node[3, 0]}, _sum);            
+            return new Mul(node[3,1], sum);
         }
 
         public static bool CoefficientDivisionRunnable(RPN.Node node)
@@ -114,9 +102,7 @@ namespace AbMath.Calculator.Simplifications
         public static RPN.Node CoefficientDivision(RPN.Node node)
         {
             RPN.Node sum = new RPN.Node(new RPN.Node[] { node[0].Clone(), node[1].Clone(), node[2].Clone(), node[3, 1] }, _sum);
-            RPN.Node division = new RPN.Node(new RPN.Node[] {node[3,0], sum}, new RPN.Token("/",2,RPN.Type.Operator));
-
-            return division;
+            return new Div(sum ,node[3,0]);
 
         }
 
@@ -154,29 +140,24 @@ namespace AbMath.Calculator.Simplifications
                 RPN.Node j = new RPN.Node(i);
                 RPN.Node subtraction = new RPN.Node(power.GetNumber() - j.GetNumber()); //(p - j)
                 RPN.Node addition = new RPN.Node(subtraction.GetNumber() + 1); //(p - j + 1)
-                RPN.Node exponent = new RPN.Node(new RPN.Node[] { addition.Clone(), end.Clone() }, _exp); //n^(p - j + 1)
 
+                RPN.Node exponent = new Pow(end.Clone(), addition.Clone()); //n^(p - j + 1)
                 RPN.Node bernoulli = Sum.getBernoulliNumber(i); //B(j)
 
-                
                 RPN.Node numerator = new RPN.Node(new RPN.Node[] { numeratorAddition.Clone() }, _fac); //(p + 1)!
 
                 RPN.Node denominatorFactorial = new RPN.Node(new RPN.Node[] { addition.Clone() }, _fac); //(p - j + 1)!
                 RPN.Node jFactorial = new RPN.Node(new RPN.Node[] { j.Clone() }, _fac); //j! 
-                RPN.Node denominator = new RPN.Node(new RPN.Node[] { denominatorFactorial, jFactorial }, _mul); // j! * (p - j + 1)!
+                RPN.Node denominator = new Mul(jFactorial, denominatorFactorial);  // j! * (p - j + 1)!
 
-                RPN.Node fraction = new RPN.Node(new RPN.Node[] { denominator, numerator }, _div);
+                RPN.Node fraction = new Div(numerator, denominator);
 
                 RPN.Node negativeOneExponent = new RPN.Node(new RPN.Node[] { j.Clone(), new RPN.Node(-1) }, _exp); //(-1)^j
 
-                RPN.Node temp = new RPN.Node(new RPN.Node[] { fraction, negativeOneExponent }, _mul);
-                RPN.Node tempTwo = new RPN.Node(new RPN.Node[] { exponent, bernoulli }, _mul);
-                RPN.Node multiplication = new RPN.Node(new RPN.Node[] { tempTwo, temp }, _mul);
+                RPN.Node multiplication = new Mul(new Mul(negativeOneExponent, fraction), new Mul(bernoulli, exponent));
                 total.AddChild(multiplication);
             }
-
-            RPN.Node division = new RPN.Node(new RPN.Node[] { numeratorAddition.Clone(), total }, _div);
-            return division;
+            return new Div(total, numeratorAddition.Clone());
         }
     }
 }
