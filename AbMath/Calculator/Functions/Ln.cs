dae0eaa6580faaceb8c1e544b860ff5f1806ff32
65001 <asathiabalan@gmail.com>
2021-10-11 10:43:09 -0400

    Added various trig functions

---
+++ AbMath/Calculator/Functions/Ln.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbMath.Calculator.Functions
+{
+    public class Ln : RPN.Node
+    {
+        private static RPN.Token ln = new RPN.Token("ln", 1, RPN.Type.Function);
+
+        public Ln(RPN.Node node) : base(new RPN.Node[] { node }, ln) { }
+    }
+}
---
+++ AbMath/Calculator/Functions/Sec.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbMath.Calculator.Functions
+{
+    public class Sec : RPN.Node
+    {
+        private static RPN.Token sec = new RPN.Token("sec", 1, RPN.Type.Function);
+
+        public Sec(RPN.Node node) : base(new RPN.Node[] { node }, sec) { }
+    }
+}
