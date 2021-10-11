9095bb4fb140ff782b32cda1dd22de7aab4383b9
65001 <asathiabalan@gmail.com>
2021-10-11 10:43:09 -0400

    Added various trig functions

---
+++ AbMath/Calculator/Functions/Cos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbMath.Calculator.Functions
+{
+    public class Cos : RPN.Node
+    {
+        private static RPN.Token cos = new RPN.Token("cos", 1, RPN.Type.Function);
+
+        public Cos(RPN.Node node) : base(new RPN.Node[] { node }, cos) { }
+    }
+}
---
+++ AbMath/Calculator/Functions/Sin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbMath.Calculator.Functions
+{
+    public class Sin : RPN.Node
+    {
+        private static RPN.Token sin = new RPN.Token("sin", 1, RPN.Type.Function);
+
+        public Sin(RPN.Node node) : base(new RPN.Node[] { node }, sin) { }
+    }
+}
